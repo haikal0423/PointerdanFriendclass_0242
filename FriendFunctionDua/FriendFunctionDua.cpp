8ff9c5e184c30cd5da65e7a21b15488f706f02ce
haikal0423 <mhaikal23azka@gmail.com>
2024-05-07 16:14:41 +0700

    Menambahkan class pelajar dan class manusia

--- FriendFunctionDua/FriendFunctionDua.cpp
+++ FriendFunctionDua/FriendFunctionDua.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class pelajar;
+class manusia
+{
+    public:
+    void showNilaiPelajar(pelajar& x);
+};
+
+class pelajar
+{
+    private:
+    int nilai;
+
+    public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
