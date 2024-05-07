01e74474fd47af363d211f3f30806b64c7a63693
haikal0423 <mhaikal23azka@gmail.com>
2024-05-07 15:18:14 +0700

    Menambahkan class segitiga

--- Pointerandfriendclass/Pointerandfriendclass.cpp
+++ Pointerandfriendclass/Pointerandfriendclass.cpp
@@ -11,3 +11,20 @@ public:
         return a * b;
     }
 };
+
+class Segitiga
+{
+public:
+    int alas, tinggi;
+
+    int luasSegitiga(int a, int t)
+    {
+        return 0.5 * a * t;
+    }
+
+    int hitung(persegiPanjang p)
+    {
+        return p.panjang;
+    }
+};
+
