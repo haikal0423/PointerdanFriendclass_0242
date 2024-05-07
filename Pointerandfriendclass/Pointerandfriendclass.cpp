e4c90a7dbb32aa0207130e4782a51d822b08eafa
haikal0423 <mhaikal23azka@gmail.com>
2024-05-07 15:17:35 +0700

    Menambahkan class persegi panjang

--- Pointerandfriendclass/Pointerandfriendclass.cpp
+++ Pointerandfriendclass/Pointerandfriendclass.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class persegiPanjang
+{
+public:
+    int panjang, lebar;
+
+    int luasPersegiPanjang(int b, int a)
+    {
+        return a * b;
+    }
+};
