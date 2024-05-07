822e9d68925ae61bace5ab1254e44be4db9ab335
haikal0423 <mhaikal23azka@gmail.com>
2024-05-07 15:57:54 +0700

    Menambahkan void orang dan siswa

--- FriendClassSatu/FriendClassSatu.cpp
+++ FriendClassSatu/FriendClassSatu.cpp
@@ -26,3 +26,13 @@ void siswa::displayAll(orang& a)
 {
     cout << id << endl << a.nama;
 }
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+
+void siswa::setId(int pId)
+{
+    id = pId;
+}
