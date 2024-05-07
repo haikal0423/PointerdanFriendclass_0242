8f7447a91f501dc918d4676f25ae808b73b779cf
haikal0423 <mhaikal23azka@gmail.com>
2024-05-07 15:26:21 +0700

    Menambahkan int main

--- Pointerandfriendclass2/Pointerandfriendclass2.cpp
+++ Pointerandfriendclass2/Pointerandfriendclass2.cpp
@@ -12,3 +12,18 @@ public:
     }
 };
 
+int main()
+{
+    mahasiswa mhs{ 1 };  // Object mhs
+    mhs.showNim();  // member access operator
+
+    mahasiswa& refMhs = mhs;  // pointer reference refMhs
+    refMhs.nim = 2; // member access operator
+    mhs.showNim();
+
+    mahasiswa* pMhs = &mhs;  // pointer dereference pMhs
+    pMhs->nim = 3;  // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
+
